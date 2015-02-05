26076b44f7b9203a3724f7b1d3038d579b6ee5b5
Felipe Gutierrez <fgutierrez3@wisc.edu>
2015-02-05 16:19:39 -0600

    Temp Token based authentication added. Template webpage for all tests created. TODO: The token expires at one point. Need to check for that..

--- app/views.py
+++ app/views.py
@@ -4,18 +4,23 @@ import json
 from requests.auth import HTTPBasicAuth
 from app import app
 
-
-
 @app.route('/')
 @app.route('/index')
+@app.route('/index/')
 def index():
 
-    json_data = open(app.config['PATH'],'r')
-    data = json.load(json_data)
-    json_data.close()
+    url = "http://localhost:5000/chrono_test/api/"
+    if(app.config['CURRENT_TOKEN'] == None):
+        json_data = open(app.config['PATH'],'r')
+        data = json.load(json_data)
+        json_data.close()
+        response = requests.get(url + "token",
+                                auth = HTTPBasicAuth(data["username"], data["pw"]))
+        app.config['CURRENT_TOKEN'] = response.json()['token']
 
-    tests = requests.get("http://localhost:5000/chrono_test/api/tests/benchmark_ChBody",
-        auth = HTTPBasicAuth(data["username"], data["pw"]))
+
+    tests = requests.get(url + "tests", 
+                         auth = HTTPBasicAuth(app.config['CURRENT_TOKEN'], ""))
 
     title = 'Testing Infrastructure for Chrono'
     user = app.config['HELLO']
@@ -27,6 +32,26 @@ def index():
             tests = tests.json())
     else:
         return render_template('index.html',
+            title = title,
+            user = "ERROR " + str(tests.status_code))
+
+@app.route('/tests/<test_name>')
+@app.route('/tests/<test_name>/')
+def test(test_name):
+
+    url = "http://localhost:5000/chrono_test/api/tests/" + test_name
+    tests = requests.get(url, auth = HTTPBasicAuth(app.config['CURRENT_TOKEN'], ""))
+
+    title = 'Testing Infrastructure for Chrono'
+    user = app.config['HELLO']
+
+    if(tests.status_code == 200):
+        return render_template('test.html',
+            title = title,
+            user = user,
+            tests = tests.json())
+    else:
+        return render_template('test.html',
             title = title,
             user = "ERROR " + str(tests.status_code))
 
--- config_example.py
+++ config_example.py
@@ -1,1 +1,2 @@
 PATH = 'PATH json file with the username and password.'
+CURRENT_TOKEN = ''
